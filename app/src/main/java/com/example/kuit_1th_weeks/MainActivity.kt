914344ac1ec20faa687c2574dea441d9f0aeedf8
ikseong00 <iksung1289@konkuk.ac.kr>
2025-03-11 13:15:39 +0900

    init

---
+++ app/src/main/java/com/example/kuit_1th_weeks/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.kuit_1th_weeks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kuit_1th_weeks.ui.theme.KUIT_1th_weeksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KUIT_1th_weeksTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ProfileCard(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileCard(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileCardPreview() {
+    KUIT_1th_weeksTheme {
+        ProfileCard()
+    }
+}
