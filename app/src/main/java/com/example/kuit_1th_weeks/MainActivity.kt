e0262f390c9473be7eeb69f9c02b35d9a12a48cf
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-03-14 01:11:53 +0900

    [feat]: 생명주기 로그 추가

--- app/src/main/java/com/example/kuit_1th_weeks/MainActivity.kt
+++ app/src/main/java/com/example/kuit_1th_weeks/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.example.kuit_1th_weeks
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kuit_1th_weeks.ui.theme.KUIT_1th_weeksTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate")
         enableEdgeToEdge()
         setContent {
             KUIT_1th_weeksTheme {
@@ -25,11 +27,40 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestart")
+    }
 }
 
 @Composable
 fun ProfileCard(modifier: Modifier = Modifier) {
-
 }
 
 @Preview(showBackground = true)
