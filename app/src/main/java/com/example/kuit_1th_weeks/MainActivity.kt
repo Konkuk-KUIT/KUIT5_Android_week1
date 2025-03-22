1ceb658fbf16e22a329ea48fb53e3d6fabeecad2
siusiubeom <siubeom200591ugmaie.com>
2025-03-23 01:19:12 +0900

    did something

--- app/src/main/java/com/example/kuit_1th_weeks/MainActivity.kt
+++ app/src/main/java/com/example/kuit_1th_weeks/MainActivity.kt
@@ -5,13 +5,32 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kuit_1th_weeks.ui.theme.KUIT_1th_weeksTheme
+import org.w3c.dom.Text
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
 
@@ -28,35 +47,192 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("MainActivity", "onStart")
+
+}
+
+@Composable
+fun Sample() {
+    Column {
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = 27.dp,
+                    start = 29.dp
+                )
+                .size(60.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = Color.Black)
+        )
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = 20.dp,
+                    start = 29.dp
+                )
+                .size(60.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .border(
+                    width = 2.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(20.dp)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = 20.dp,
+                    start = 29.dp
+                )
+                .size(76.dp)
+                .border(
+                    width = 2.dp,
+                    color = Color.Black
+                )
+                .padding(8.dp)
+                .size(60.dp)
+                .background(color = Color.Red)
+        )
+        Image(
+            painter = painterResource(R.drawable.image),
+            contentDescription = "dog image",
+            modifier = Modifier
+                .padding(top = 20.dp, start = 29.dp)
+                .size(60.dp)
+        )
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("MainActivity", "onResume")
+}
+
+@Composable
+fun CircleSample() {
+    Column {
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = 27.dp,
+                    start = 29.dp
+                )
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(color = Color.Black)
+        )
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = 20.dp,
+                    start = 29.dp
+                )
+                .size(60.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 2.dp,
+                    color = Color.Black,
+                    shape = CircleShape
+                )
+        )
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = 20.dp,
+                    start = 29.dp
+                )
+                .size(76.dp)
+                .border(
+                    width = 2.dp,
+                    color = Color.Black
+                )
+                .padding(8.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(color = Color.Red)
+        )
+        Image(
+            painter = painterResource(R.drawable.image),
+            contentDescription = "dog image",
+            modifier = Modifier
+                .padding(top = 20.dp, start = 29.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+        )
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d("MainActivity", "onPause")
+}
+
+@Composable
+fun Profile() {
+    Box(
+        modifier = Modifier
+            .padding(
+                top = 45.dp,
+                start = 45.dp
+            )
+            .size(140.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(20.dp)
+            )
+    )
+    Column{
+        Image(
+            painter = painterResource(R.drawable.image),
+            contentDescription = "내 사진",
+            modifier = Modifier
+                .padding(top = 55.dp, start = 55.dp)
+                .size(40.dp)
+                .clip(RoundedCornerShape(10.dp))
+        )
+        Text(
+            text = "이름: 범시우",
+            fontSize = 8.sp,
+            style = androidx.compose.ui.text.TextStyle(
+                platformStyle = PlatformTextStyle(includeFontPadding = false)
+            ),
+            modifier = Modifier.
+            padding(
+                top = 5.dp,
+                start = 55.dp
+            )
+        )
+        Text(
+            text = "학번 : 202412950",
+            fontSize = 8.sp,
+            style = androidx.compose.ui.text.TextStyle(
+                platformStyle = PlatformTextStyle(includeFontPadding = false)
+            ),
+            modifier = Modifier.
+            padding(
+                top = 5.dp,
+                start = 55.dp
+            )
+        )
+        Text(
+            text = "학과: 수의예과",
+            fontSize = 8.sp,
+            style = androidx.compose.ui.text.TextStyle(
+                platformStyle = PlatformTextStyle(includeFontPadding = false)
+            ),
+            modifier = Modifier.
+            padding(
+                top = 5.dp,
+                start = 55.dp
+            )
+        )
+        Text(
+            text = "생년월일 : 2005/09.15",
+            fontSize = 8.sp,
+            style = androidx.compose.ui.text.TextStyle(
+                platformStyle = PlatformTextStyle(includeFontPadding = false)
+            ),
+            modifier = Modifier.
+            padding(
+                top = 5.dp,
+                start = 55.dp
+            )
+        )
     }
 
-    override fun onStop() {
-        super.onStop()
-        Log.d("MainActivity", "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("MainActivity", "onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("MainActivity", "onRestart")
-    }
 }
 
 @Composable
@@ -67,6 +243,6 @@ fun ProfileCard(modifier: Modifier = Modifier) {
 @Composable
 fun ProfileCardPreview() {
     KUIT_1th_weeksTheme {
-        ProfileCard()
+        Profile()
     }
 }
